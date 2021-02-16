172781fbaa4046da78de2dd17d055a78cf06bcfd
andrei <b1naryth1ef@gmail.com>
2021-02-16 13:51:45 -0800

    oops

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"path"
 	"path/filepath"
 
-	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_proxy/v3"
+	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/ratelimit/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/redis_proxy/v3"
 	_ "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/tcp_proxy/v3"
